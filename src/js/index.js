d244ae0f46cae7741a7093b4ee27214bcb993962
Egor Ahmetov <ahmetov.egor96@mail.ru>
2024-02-22 14:38:36 +0300

    create chekets

--- src/js/index.js
+++ src/js/index.js
@@ -24,3 +24,40 @@ function openSortMenu () {
 	})
 }
 openSortMenu()
+
+
+
+const cheketBlocks = document.querySelectorAll('.cheket__block')
+
+
+
+// cheketBlocks.forEach(el => {
+// 	el.addEventListener('click', function (e) {
+// 		const self = e.currentTarget
+// 		const header = self.querySelector('.cheket__header')
+// 		const body = self.querySelector('.cheket__body')
+
+// 		self.classList.toggle('cheket__body--active')
+// 	})
+// })
+
+// function openCheketBody () {
+
+// }
+// openCheketBody()
+
+
+// const filterItem = document.querySelectorAll('.filter__item')
+// const filterBtns = document.querySelectorAll('.filter-btn__close')
+
+
+// function deleteFilterItem () {
+// 	filterItem.forEach(item => {
+// 		item.addEventListener('click', function (e) {
+// 			filterBtns.forEach(btn => {
+
+// 			})
+// 		})
+// 	})
+// }
+// deleteFilterItem()
